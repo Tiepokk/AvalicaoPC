082786a565af58277da208adff1be1c869c8e8d8
Lucas Tiepo <144742308+Tiepokk@users.noreply.github.com>
2026-04-27 17:30:51 -0300

    Alteração feita no .cmd para que os arquivos compilados fiquem na pasta out

--- start_execution.cmd
+++ start_execution.cmd
@@ -1,31 +1,33 @@
 @echo off
 title Sistema de Producao de Veiculos
 
-cd src
+if not exist "out" mkdir "out"
 
-echo Compilando arquivos...
-javac veiculo/*.java fabrica/*.java loja/*.java cliente/*.java
+echo Compilando arquivos para a pasta out...
+javac -d out -sourcepath src src/fabrica/*.java src/loja/*.java src/cliente/*.java src/veiculo/*.java
+
 if %errorlevel% neq 0 (
     echo Erro na compilacao. Verifique o codigo.
     pause
     exit /b
 )
 
-start "FABRICA" cmd /k "java fabrica.Fabrica"
+echo Iniciando componentes...
+
+start "FABRICA" cmd /k "java -cp out fabrica.Fabrica"
 
 timeout /t 2 /nobreak > nul
 
-start "LOJA 1" cmd /k "java loja.LojaMain 1"
-start "LOJA 2" cmd /k "java loja.LojaMain 2"
-start "LOJA 3" cmd /k "java loja.LojaMain 3"
+start "LOJA 1" cmd /k "java -cp out loja.LojaMain 1"
+start "LOJA 2" cmd /k "java -cp out loja.LojaMain 2"
+start "LOJA 3" cmd /k "java -cp out loja.LojaMain 3"
 
 timeout /t 2 /nobreak > nul
 
-start "CLIENTES" cmd /k "java cliente.Cliente"
+start "CLIENTES" cmd /k "java -cp out cliente.Cliente"
 
 echo ==========================================
 echo Sistema distribuido iniciado com sucesso!
 echo ==========================================
 
-cd ..
 pause
